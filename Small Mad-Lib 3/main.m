51eb0adb20e65df75aae110f484bd5dbccc71b23
Edward Barber <luckyshot423@gmail.com>
2016-08-28 18:31:22 -0400

    Initial Commit

---
+++ Small Mad-Lib 3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Small Mad-Lib 3
+//
+//  Created by Andrew Barber on 8/28/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
