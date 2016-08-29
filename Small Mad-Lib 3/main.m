9611d70f7429d4f9ed523ed8e91cad9da4de7113
Edward Barber <luckyshot423@gmail.com>
2016-08-28 21:48:50 -0400

    Not working but need a checkpoint.

--- Small Mad-Lib 3/main.m
+++ Small Mad-Lib 3/main.m
@@ -8,10 +8,71 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    Bright,
+    Big,
+    Strange
+}Adjectives;
+
+
+//--------------------------character name start------------------------------------------------
+
+typedef struct PlayerCharacter {
+    char *name;
+}PlayerCharacter;
+
+bool getStringFromUser(char name[], int nameLength, const char *prompt);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        PlayerCharacter user;
+        user.name = malloc(sizeof(char) * 50);
+        
+        bool haveName = false;
+        while (!haveName) {
+            haveName = getStringFromUser(user.name, 50, "What is your name?\n");
+        }
+        
+        NSLog(@"Hello %s\n", user.name);
+        
+//-------------------------character name end---------------------------------------------------
+        
+        NSLog(@"Pick an adjective!\n");
+        NSLog(@"1: Bright\n2: Big\n3: Strange\n");
+        Adjectives userChoice = 1;
+        
+            switch (userChoice) {
+                case Bright: {
+                    NSLog(@"Bright");
+                    break;
+            }
+                case Big: {
+                    NSLog(@"Big");
+                    break;
+                }
+                case Strange: {
+                    NSLog(@"Strange");
+                    break;
+                }
+                default:
+                    NSLog(@"Oh no!");
+                    break;
+            }
+        
+        NSLog(@"You chose %i!\n", userChoice);
+        
     }
     return 0;
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
