35868a1f420990414d799506c2e86c2ca013fc46
Andrew Barber <edwardbar423@gmail.com>
2016-08-29 23:15:18 -0400

    Finished

--- Small Mad-Lib 3/main.m
+++ Small Mad-Lib 3/main.m
@@ -9,19 +9,30 @@
 #import <Foundation/Foundation.h>
 
 typedef enum {
-    Bright,
+    Bright = 1,
     Big,
     Strange
 }Adjectives;
 
+typedef enum {
+    House = 1,
+    Car,
+    Shoe
+}Nouns;
 
-//--------------------------character name start------------------------------------------------
+typedef enum {
+    Run = 1,
+    Tumble,
+    Spew
+}Verbs;
 
 typedef struct PlayerCharacter {
     char *name;
 }PlayerCharacter;
 
 bool getStringFromUser(char name[], int nameLength, const char *prompt);
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -37,42 +48,101 @@ int main(int argc, const char * argv[]) {
         
         NSLog(@"Hello %s\n", user.name);
         
-//-------------------------character name end---------------------------------------------------
-        
+
         NSLog(@"Pick an adjective!\n");
         NSLog(@"1: Bright\n2: Big\n3: Strange\n");
-        Adjectives userChoice = 1;
+        Adjectives userChoiceAdjective = -1;
+        scanf("%d", &userChoiceAdjective);
         
-            switch (userChoice) {
-                case Bright: {
-                    NSLog(@"Bright");
-                    break;
+        NSString *stringChoiceAdjectives = @"";
+        
+        switch (userChoiceAdjective) {
+            case Bright: {
+                stringChoiceAdjectives = @"Bright";
+                break;
             }
-                case Big: {
-                    NSLog(@"Big");
-                    break;
-                }
-                case Strange: {
-                    NSLog(@"Strange");
-                    break;
-                }
-                default:
-                    NSLog(@"Oh no!");
-                    break;
+            case Big: {
+                stringChoiceAdjectives = @"Big";
+                break;
             }
+            case Strange: {
+                stringChoiceAdjectives = @"Strange";
+                break;
+            }
+            default:
+                NSLog(@"Oh no!");
+                break;
+        }
         
-        NSLog(@"You chose %i!\n", userChoice);
+        NSLog(@"You chose %@!\n", stringChoiceAdjectives);
+        NSLog(@"Now, pick a noun\n");
+        NSLog(@"1: House\n2: Car\n3: Shoe\n");
+        Nouns userChoiceNoun = -1;
+        scanf("%d", &userChoiceNoun);
         
+        NSString *stringChoiceNouns = @"";
+        
+        switch (userChoiceNoun) {
+            case House:
+                stringChoiceNouns = @"House";
+                break;
+            case Car:
+                stringChoiceNouns = @"Car";
+                break;
+            case Shoe:
+                stringChoiceNouns = @"Shoe";
+                break;
+            default:
+                NSLog(@"Oh no!");
+                break;
+        }
+        NSLog(@"You chose %@!\n", stringChoiceNouns);
+        NSLog(@"Now pick a verb\n");
+        NSLog(@"1: Run\n2: Tumble\n3: Spew\n");
+        Verbs userChoiceVerb = -1;
+        scanf("%d", &userChoiceVerb);
+        
+        NSString *stringChoiceVerbs = @"";
+        
+        switch (userChoiceVerb) {
+            case Run:
+                stringChoiceVerbs = @"Run";
+                break;
+            case Tumble:
+                stringChoiceVerbs= @"Tumble";
+                break;
+            case Spew:
+                stringChoiceVerbs = @"Spew";
+                break;
+            default:
+                NSLog(@"Oh no!");
+                break;
+        }
+        NSLog(@"You chose %@!\n", stringChoiceVerbs);
+        NSLog(@"Here is your story\n");
+        NSLog(@"Once upon a time, %s was a mighty warrior. They were known for their %@ demeanor. A particular thing that helped their heroic deeds was a %@ though no one knew why. When they became relentless they could not help but use their %@ abiliy.", user.name, stringChoiceAdjectives, stringChoiceNouns, stringChoiceVerbs);
     }
     return 0;
 }
+
+bool getStringFromUser(char name[], int nameLength, const char *prompt) {
+    
+    char *result = NULL;
+    while (result != name) {
+        fpurge(stdin);
+        printf("%s", prompt);
+        result = fgets(name, nameLength, stdin);
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
+    // we will change this
+    return true;
+}
+
+
+
+
+
+
+
+
